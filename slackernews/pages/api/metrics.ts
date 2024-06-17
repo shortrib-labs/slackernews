f4163d0125f601ceb0b8ec360c183218ce53d1b7
Chuck D'Antonio <chuck@replicated.com>
2024-06-17 17:17:03 -0400

    Restores variables for metrics and settles on 200

--- slackernews/pages/api/metrics.ts
+++ slackernews/pages/api/metrics.ts
@@ -4,5 +4,5 @@ import { sendMetrics } from "../../lib/metrics/metric";
 export default async function handler(_: NextApiRequest, res: NextApiResponse) {
   // send custom metrics to replicated metrics when sending prometheus metrics
   sendMetrics();
-  res.status(201).end();
+  res.status(200).end();
 }
