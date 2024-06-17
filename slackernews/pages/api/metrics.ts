02060ad468629d1236a4349dcb9956680b5b1f94
Chuck D'Antonio <chuck@replicated.com>
2024-06-17 23:28:14 +0200

    Provides metrics on usage (#9)
    TL;DR
    -----
    
    Sends custom metrics for daily and monthly users
    
    Details
    -------
    
    Adds support for custom metrics to keep track of customer usage against their
    license. An update is sent on load of the home page and once a day via a
    cron job.

---
+++ slackernews/pages/api/metrics.ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { sendMetrics } from "../../lib/metrics/metric";
+
+export default async function handler(_: NextApiRequest, res: NextApiResponse) {
+  // send custom metrics to replicated metrics when sending prometheus metrics
+  sendMetrics();
+  res.status(200).end();
+}
