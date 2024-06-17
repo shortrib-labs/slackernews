07f19ab2de40fbc9b7057c28ee3f429333d10963
Chuck D'Antonio <chuck@replicated.com>
2024-06-17 16:54:27 -0400

    Simplifies metrics page

--- slackernews/pages/api/metrics.ts
+++ slackernews/pages/api/metrics.ts
@@ -1,20 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { register, collectDefaultMetrics } from 'prom-client';
 import { sendMetrics } from "../../lib/metrics/replicated";
-import { collectUserMetrics, collectLicenseEntitlements, collectVersionMetrics } from "../../lib/metrics/prometheus";
-
-collectDefaultMetrics({ prefix: 'slackernews_' });
 
 export default async function handler(_: NextApiRequest, res: NextApiResponse) {
   // send custom metrics to replicated metrics when sending prometheus metrics
   sendMetrics();
-  
-  // prepare prometheus metrics
-  collectUserMetrics();
-  collectLicenseEntitlements();
-  collectVersionMetrics();
-
-  res.setHeader('Content-type', register.contentType);
-  res.setHeader('Cache-Control', 'no-store');
-  res.send(await register.metrics());
+  res.status(201).end();
 }
